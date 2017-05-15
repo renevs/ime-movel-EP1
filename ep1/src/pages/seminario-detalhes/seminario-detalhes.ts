68dc6e604472616bd532863ec38907197db49f78
Ronaldo Yang <ronaldo.yang@yahoo.com.br>
2017-05-15 08:02:06 -0300

    Adicionando o restante das funcionalidades

--- ep1/src/pages/seminario-detalhes/seminario-detalhes.ts
+++ ep1/src/pages/seminario-detalhes/seminario-detalhes.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { PresencaService } from '../../services/presenca.service';
+import { AlunoService } from '../../services/aluno.service';
+import { Aluno } from '../../entities/aluno';
 
 @IonicPage()
 @Component({
@@ -11,24 +13,29 @@ export class SeminarioDetalhesPage {
   seminarioId: string = this.navParams.get('seminarioId');
   seminarioName: string = this.navParams.get('seminarioName');
   type: string = this.navParams.get('type');
+  alunos: Array<Aluno> = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private presencaService: PresencaService) {
-    //TODO
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alunoService: AlunoService, private presencaService: PresencaService) {
+    this.presencaService.listAlunos(this.seminarioId).then((presenca) => {
+      for (let p of presenca.data) {
+        this.alunoService.searchAluno(p.student_nusp).then((aluno) => this.alunos.push(aluno));           
+      }
+    });     
   }
 
   generateQRCode() {
-//TODO
+  //TODO
   }
 
   listenBluetooth() {
-//TODO
+  //TODO
   }
 
   confirmQRCode() {
-//TODO
+  //TODO
   }
 
   confirmBluetooth() {
-//TODO
+  //TODO
   }
 }
--- ep1/src/pages/seminario/seminario.ts
+++ ep1/src/pages/seminario/seminario.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Events, MenuController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Events, MenuController, AlertController } from 'ionic-angular';
 import { SeminarioService } from '../../services/seminario.service';
 import { ProfessorService } from '../../services/professor.service';
 import { AlunoService } from '../../services/aluno.service';
@@ -17,14 +17,18 @@ export class SeminarioPage {
   nome: string = '';
   type: string = this.navParams.get('type');
 
-  constructor(private storage: Storage, public events: Events, public navCtrl: NavController, public navParams: NavParams, public menu: MenuController, private seminarioService: SeminarioService, private alunoService: AlunoService, private professorService: ProfessorService) {
+  constructor(private alertCtrl: AlertController, private storage: Storage, public events: Events, public navCtrl: NavController, public navParams: NavParams, public menu: MenuController, private seminarioService: SeminarioService, private alunoService: AlunoService, private professorService: ProfessorService) {
+    this.getSeminarios();
+    this.getUserName(this.type);            
+  }
+
+  getSeminarios() {
     this.seminarioService
       .getSeminario()
       .then(seminarios => {
                             this.seminarios = seminarios;
                           } ,
           error => alert(error));
-    this.getUserName(this.type);            
   }
 
   getUserName(type: string) {
@@ -47,8 +51,34 @@ export class SeminarioPage {
   }
   
   addSeminario() {
-    //TODO
-  }
+      let prompt = this.alertCtrl.create({
+        title: "Adicionar um Seminário",
+        inputs: [
+          {
+            name: 'seminarioName',
+            placeholder: 'Nome do Seminário'
+          },
+        ],
+        buttons: [
+          {
+            text: 'Cancelar'
+          },
+          {
+            text: 'Ok',
+            handler: data => {
+              this.seminarioService.addSeminario(data.seminarioName).then((response) => {
+                if (response.success) {
+                  console.log('add');
+                  this.getSeminarios();
+                }
+                else console.log('nops');
+              });
+            }
+          }
+        ]
+      });
+      prompt.present();
+  }   
 
   goToDetails(seminario: Seminario) {
     this.navCtrl.push(SeminarioDetalhesPage, {
